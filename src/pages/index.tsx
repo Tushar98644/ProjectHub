3696d9b97b1e155265d96e69ee5cdfb435459f38
Tushar98644 <b.toshar@iitg.ac.in>
2023-06-27 10:47:49 +0530

    github provider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,107 @@
+'use client'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import { signIn, useSession } from 'next-auth/react'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 const Home = () => {
-  const {data:session} = useSession();
+  const { data: session } = useSession();
+  // returned object is being destructured to extract the data property. Then, the data property is being aliased as session.
 
   if (!session) {
     return (
-      <>
-        <h1>not session</h1>
-        <div>
-          <button onClick={signIn('google')}>sign in
-          </button>
-        </div>
-      </>
+      <div>
+        <main
+          className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
+          <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+            <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+              Get started by editing&nbsp;
+              <code className="font-mono font-bold">src/pages/index.tsx</code>
+            </p>
+          </div>
+          <div className="bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% ...">
+            <button onClick={() => signIn('google')}>sign in
+            </button>
+          </div>
+          <div>
+            <button onClick={() => signIn('github')}>sign in
+            </button>
+          </div>
+          <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
+            <div className="text-5xl font-regular">
+              Project Hub
+            </div>
+          </div>
+          <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+            <a
+              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+              className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h2 className={`mb-3 text-2xl font-semibold`}>
+                Docs{' '}
+                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                  -&gt;
+                </span>
+              </h2>
+              <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+                Find in-depth information about Next.js features and API.
+              </p>
+            </a>
+
+            <a
+              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+              className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h2 className={`mb-3 text-2xl font-semibold`}>
+                Learn{' '}
+                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                  -&gt;
+                </span>
+              </h2>
+              <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+              </p>
+            </a>
+
+            <a
+              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+              className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h2 className={`mb-3 text-2xl font-semibold`}>
+                Templates{' '}
+                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                  -&gt;
+                </span>
+              </h2>
+              <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+                Discover and deploy boilerplate example Next.js&nbsp;projects.
+              </p>
+            </a>
+            <a
+              href=""
+              className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h2 className={`mb-3 text-2xl font-semibold`}>
+                Deploy{' '}
+                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                  -&gt;
+                </span>
+              </h2>
+              <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+                Instantly deploy your Next.js site to a shareable URL with Vercel.
+              </p>
+            </a>
+          </div>
+        </main>
+      </div>
     )
   }
 
@@ -118,6 +204,9 @@ const Home = () => {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
+        <div>
+          <button onClick={() => signOut()}>sign out</button>
+        </div>
       </div>
     </main>
   )
