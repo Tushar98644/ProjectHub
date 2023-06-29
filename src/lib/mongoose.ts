14177bd696801577b6f3f42c3ec1d604d3413cc0
Tushar98644 <b.toshar@iitg.ac.in>
2023-06-29 11:57:56 +0530

    mogoose connection types

--- src/lib/mongoose.ts
+++ src/lib/mongoose.ts
@@ -1,16 +1,18 @@
-import mongoose from "mongoose";
-
-interface UriType {
-    uri: string | undefined;
-}
+import mongoose, { ConnectOptions } from "mongoose";
 
 const mongooseconnect = async () => {
+  const readyState = mongoose.connections[0].readyState as ConnectionState;
 
-    if (mongoose.connections[0].readyState === 1)
-        return mongoose.connections[0].asPromise();
-    
-    const uri = process.env.MONGODB_URI;
-    return mongoose.connect(uri);
+  if (readyState === 1) {
+    return mongoose.connections[0].asPromise();
+  }
+
+  const uri = process.env.MONGODB_URI as string;
+  const options: ConnectOptions = {
+    // Specify any connection options you need here
+  };
+
+  return mongoose.connect(uri, options);
 };
 
 export default mongooseconnect;
