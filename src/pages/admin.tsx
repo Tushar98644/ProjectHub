2d099f74a69cd8e5eda00039cd11ca798c7413c2
Tushar98644 <b.toshar@iitg.ac.in>
2023-06-30 00:34:55 +0530

    Admin page animation

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,14 +1,16 @@
 import { Navbar } from "@/components";
-import { HI, Welcome } from "@/shared";
+import { HI } from "@/shared";
 import Lottie from "lottie-react";
 
 const Admin = () => {
     return (
         <div>
             <Navbar />
-            <div className="p-48 text-nav-text text-4xl font-bold flex flex-row gap-10">
-                <h1>welcome</h1>
-                <Lottie animationData={HI} height={50} width={50} />
+            <div className="p-48 text-nav-text text-5xl font-bold flex flex-row items-center gap-0">
+                <div className="w-[15vw]">
+                    <Lottie animationData={HI} height={50} width={50} className=""/>
+                </div>
+                <p>welcome </p>
             </div>
         </div>
     );
