051387144d3880ea42e4fcc7041e702ccf617f83
Tushar98644 <b.toshar@iitg.ac.in>
2023-07-11 15:26:33 +0530

    added contributor details in project card

--- src/components/Card/card.tsx
+++ src/components/Card/card.tsx
@@ -4,9 +4,12 @@ import { Project } from "@/types/Project";
 import Link from "next/link";
 import { motion } from "framer-motion";
 import Tilt from "react-parallax-tilt";
+import { useSession } from "next-auth/react";
 
 
 const Card = ({ title, description, image, github }: Project) => {
+
+    const { data: Session } = useSession();
     return (
         <motion.div>
             <Tilt tiltReverse={true} tiltMaxAngleX={20} tiltMaxAngleY={10} gyroscope={true} scale={1.05} transitionSpeed={1500}
@@ -28,6 +31,7 @@ const Card = ({ title, description, image, github }: Project) => {
                             <h5 className="mb-2 sm:text-2xl text-base font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
                         </a>
                         <p className="mb-3 font-normal sm:text-base text-sm text-gray-700 dark:text-gray-400 text-ellipsis overflow-auto">{description}</p>
+                        <p className="mb-3 font-normal sm:text-base text-sm text-black dark:text-white text-ellipsis overflow-auto">Contributor : {Session?.user?.name}</p>
                         <a href={github} className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                             <p> view github</p>
                             <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
