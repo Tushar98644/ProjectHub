2be5fc2821fc91a032b0923de022f3ba731e64ce
Tushar98644 <b.toshar@iitg.ac.in>
2023-07-18 13:55:20 +0530

    chatbot

---
+++ src/components/chatbot/chatbot.tsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import { useChat } from 'ai/react';
+import Image from 'next/image';
+
+const Chat = () => {
+  const [isChatOpen, setIsChatOpen] = useState(false);
+
+  const toggleChat = () => {
+    setIsChatOpen((prevState) => !prevState);
+  };
+
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
+
+  return (
+    <div className='text-gray-500'>
+      <div
+        className="fixed bottom-6 right-10 cursor-pointer z-50"
+        onClick={toggleChat}>
+        <Image src="/chatbot.webp" alt="Chatbot Icon" className="w-20 h-20" width={100} height={100} />
+      </div>
+      {isChatOpen && (
+        <div className="fixed bottom-24 right-10 z-40">
+          <div className="bg-white rounded-lg shadow-lg w-500 h-400 p-4">
+            <div className="flex-1 overflow-y-auto">
+              {messages.map((m) => (
+                <div key={m.id} className="mb-2">
+                  <span className="font-bold">{m.role}:</span>
+                  <span className="ml-2">{m.content}</span>
+                </div>
+              ))}
+            </div>
+            <form className="flex mt-2" onSubmit={handleSubmit}>
+              <input
+                className="flex-1 px-2 py-1 border border-gray-300 rounded-md"
+                value={input}
+                placeholder="Ask anything..."
+                onChange={handleInputChange}
+              />
+              <button
+                type="submit"
+                className="ml-2 px-4 py-2 bg-blue-500 text-white rounded-md"
+              >
+                Send
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Chat;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import { SessionProvider } from 'next-auth/react'
 import { AuthenticationGuard } from '@/providers';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import {Layout} from '@/components';
+import {Layout,ChatBot} from '@/components';
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
@@ -12,6 +12,7 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
       <AuthenticationGuard>
         <ToastContainer />
         <Layout/>
+        <ChatBot/>
         {/* <div className="video-background"> */}
           {/* <video playsInline autoPlay muted loop>
             <source src='/dark-bg-video.mp4' type="video/mp4" />
