8472e7aae11d7f7f00f17ef5f5790c2abcdc1f05
Dhruv127 <dhruvqagarwal@gmail.com>
2023-10-04 21:00:10 +0530

    add-responsive table

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -172,7 +172,7 @@ const Admin = () => {
                                             href="#"
                                             className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                                         >
-                                            Rewa
+                                            Reward
                                         </a>
                                     </li>
                                     <li>
