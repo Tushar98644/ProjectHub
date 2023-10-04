c89e5d00f8fb9d5869250e160f82b468c0f98142
Dhruv Agarwal <92450489+Dhruv127@users.noreply.github.com>
2023-10-04 21:13:03 +0530

    Update admin.tsx

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -152,9 +152,9 @@ const Admin = () => {
                                     xmlns="http://www.w3.org/2000/svg"
                                 >
                                     <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth="2"
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        stroke-width="2"
                                         d="M19 9l-7 7-7-7"
                                     ></path>
                                 </svg>
@@ -215,9 +215,9 @@ const Admin = () => {
                                     xmlns="http://www.w3.org/2000/svg"
                                 >
                                     <path
-                                        fillRule="evenodd"
+                                       fill-rule="evenodd"
                                         d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                                        clipRule="evenodd"
+                                        clip-rule="evenodd"
                                     ></path>
                                 </svg>
                             </div>
