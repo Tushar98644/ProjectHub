7f85c56b1522de606fd0df945f391c7f9a630613
HimanshuTembhe <himanshutembhe@gmail.com>
2023-10-14 15:34:34 +0530

    Updated changes

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -4,13 +4,6 @@ import { Fragment } from "react";
 const Layout = ({ children }: React.PropsWithChildren) => {
     return (
         <Fragment>
-            <head>
-                <link
-                    rel="icon"
-                    type="image/x-icon"
-                    href="/images/favicon.png"
-                />
-            </head>
             <Navbar />
             <ChatBot />
             {children}
