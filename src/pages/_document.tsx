9c3b6f521bba54717bec5e8aa9a70ccfdb923149
HimanshuTembhe <himanshutembhe@gmail.com>
2023-10-14 15:55:08 +0530

    Updated changes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,9 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
     return (
         <Html lang="en">
-            <Head>
-                <link rel="shortcut icon" href="/images/favicon.png" />
-            </Head>
+            <Head />
             <body>
                 <Main />
                 <NextScript />
