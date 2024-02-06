21a6e26fb5dad1cd60a522c3514f789296228a24
Tushar Banik <b.toshar@iitg.ac.in>
2024-02-06 11:57:54 +0530

    discussion page fix

--- src/app/discussion/[id]/page.tsx
+++ src/app/discussion/[id]/page.tsx
@@ -61,10 +61,6 @@ const Discussion_page = ({ params }: { params: { id: string } }) => {
         await axios
             .get(
                 apiUrl,
-                //     {
-                //     params: {id},
-                //     ...config,
-                // }
                 config
             )
             .then(res => {
@@ -84,7 +80,7 @@ const Discussion_page = ({ params }: { params: { id: string } }) => {
         <div>
             {!showdiscussion ? (
                 <div className="flex items-center justify-center h-screen px-8">
-                    <div className="flex justify-center p-8 items-center flex-col rounded-xl bg-green-50 text-slate-800 shadow-sm dark:bg-slate-900 dark:text-slate-400">
+                    <div className="flex justify-center p-8 items-center flex-col rounded-xl bg-green-50 text-slate-800 shadow-sm dark:bg-slate-900 dark:text-slate-400 z-[2]">
                         <div className="inline-flex items-center">
                             <svg
                                 className="mr-2  h-6 w-6 fill-current text-teal-900 dark:text-teal-400"
@@ -116,14 +112,15 @@ const Discussion_page = ({ params }: { params: { id: string } }) => {
                     </div>
                 </div>
             ) : (
-                <div className="py-24 dark:bg-gray-900 min-h-screen">
-                    <section className="bg-white dark:bg-gray-900 antialiased py-8">
+                <div className="pt-32 flex flex-col px-60 h-screen opacity-80 dark:bg-gray-900 z-[2]">
+                    <div className="bg-white dark:bg-gray-900 antialiased py-8 z-[2]">
                         <div className="max-w-2xl mx-auto px-4 ">
                             <div className="flex justify-between items-center mb-6">
                                 <h2 className="text-lg lg:text-2xl font-bold text-gray-900 dark:text-white">
                                     Discussions
                                 </h2>
                             </div>
+                            {/* <button className="z-[2]">hello</button> */}
                             <form className="mb-6">
                                 <div className="py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700">
                                     <label
@@ -197,7 +194,7 @@ const Discussion_page = ({ params }: { params: { id: string } }) => {
                                         </button>
                                         <div
                                             id="dropdownComment1"
-                                            className="hidden z-10 w-36 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600"
+                                            className="hidden w-36 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600"
                                         >
                                             <ul
                                                 className="py-1 text-sm text-gray-700 dark:text-gray-200"
@@ -258,110 +255,8 @@ const Discussion_page = ({ params }: { params: { id: string } }) => {
                                     </div>
                                 </article>
                             ))}
-
-                            {/* <article className="p-6 mb-3 ml-6 lg:ml-12 text-base bg-white rounded-lg dark:bg-gray-900">
-                                    <footer className="flex justify-between items-center mb-2">
-                                        <div className="flex items-center">
-                                            <p className="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold">
-                                                <img
-                                                    className="mr-2 w-6 h-6 rounded-full"
-                                                    src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-                                                    alt="Jese Leos"
-                                                />
-                                                Jese Leos
-                                            </p>
-                                            <p className="text-sm text-gray-600 dark:text-gray-400">
-                                                <time
-                                                    dateTime="2022-02-12"
-                                                    title="February 12th, 2022"
-                                                >
-                                                    Feb. 12, 2022
-                                                </time>
-                                            </p>
-                                        </div>
-                                        <button
-                                            id="dropdownComment2Button"
-                                            data-dropdown-toggle="dropdownComment2"
-                                            className="inline-flex items-center p-2 text-sm font-medium text-center text-gray-500 dark:text-gray-40 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                                            type="button"
-                                        >
-                                            <svg
-                                                className="w-4 h-4"
-                                                aria-hidden="true"
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                fill="currentColor"
-                                                viewBox="0 0 16 3"
-                                            >
-                                                <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z" />
-                                            </svg>
-                                            <span className="sr-only">
-                                                Comment settings
-                                            </span>
-                                        </button>
-                                        <div
-                                            id="dropdownComment2"
-                                            className="hidden z-10 w-36 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600"
-                                        >
-                                            <ul
-                                                className="py-1 text-sm text-gray-700 dark:text-gray-200"
-                                                aria-labelledby="dropdownMenuIconHorizontalButton"
-                                            >
-                                                <li>
-                                                    <a
-                                                        href="#"
-                                                        className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                                                    >
-                                                        Edit
-                                                    </a>
-                                                </li>
-                                                <li>
-                                                    <a
-                                                        href="#"
-                                                        className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                                                    >
-                                                        Remove
-                                                    </a>
-                                                </li>
-                                                <li>
-                                                    <a
-                                                        href="#"
-                                                        className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                                                    >
-                                                        Report
-                                                    </a>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </footer>
-                                    <p className="text-gray-500 dark:text-gray-400">
-                                        Much appreciated! Glad you liked it ☺️
-                                    </p>
-                                    <div className="flex items-center mt-4 space-x-4">
-                                        <button
-                                            type="button"
-                                            className="flex items-center text-sm text-gray-500 hover:underline dark:text-gray-400 font-medium"
-                                        >
-                                            <svg
-                                                className="mr-1.5 w-3.5 h-3.5"
-                                                aria-hidden="true"
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                fill="none"
-                                                viewBox="0 0 20 18"
-                                            >
-                                                <path
-                                                    stroke="currentColor"
-                                                    stroke-linecap="round"
-                                                    stroke-linejoin="round"
-                                                    stroke-width="2"
-                                                    d="M5 5h5M5 8h2m6-3h2m-5 3h6m2-7H2a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h3v5l5-5h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z"
-                                                />
-                                            </svg>
-                                            Reply
-                                        </button>
-                                    </div>
-                                </article> */}
                         </div>
-                    </section>
+                    </div>
                 </div>
             )}
         </div>
